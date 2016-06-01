2a1004644f3bef2bd4ba5c25ced50c9f91a60e3d
platinumgamer <platinumgamer@hotmail.com>
2016-06-01 15:32:39 +1000

    Spawner
    Bombs are added

---
+++ Group 20 Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject bomb;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Spawn ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Spawn()
+	{
+		while (true) 
+		{
+			Vector3 spawnPosition = new Vector3 (Random.Range (-1, 1), transform.position.y, 0);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (bomb, spawnPosition, spawnRotation);
+		}
+		yield return new WaitForSeconds (1);
+	}
+}
