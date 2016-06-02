01b09314fcfefce00b5c1ef6497416a4343fdd03
platinumgamer <platinumgamer@hotmail.com>
2016-06-02 10:26:29 +1000

    Health
    Ship has hp

---
+++ Group 20 Game/Assets/Scripts/ShipHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShipHealth : MonoBehaviour {
+
+	public int healthPoints;
+	public Text healthText;
+
+	void OnCollisionEnter2D (Collision2D bomb) {
+		if (bomb.gameObject.tag == "Enemy") {
+			healthPoints -= 1;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		healthText.text = "Ship HP: " + healthPoints;
+	}
+
+}
