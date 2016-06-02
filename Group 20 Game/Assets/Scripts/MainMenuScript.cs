4e5cb020c58a0f8c2e3bb7b52392b481a17dd39c
BMourtzis <vasilis.mourtzis@outlook.com>
2016-06-03 03:18:49 +1000

    Menu and others update
    Added pause menu functionality, a main menu scene and ammo reset
    functionality

--- Group 20 Game/Assets/Scripts/MainMenuScript.cs
+++ Group 20 Game/Assets/Scripts/MainMenuScript.cs
@@ -3,22 +3,17 @@ using System.Collections;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 
-public class StartMenuScript : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 {
     [SerializeField]
     Canvas LevelCanvas;
 
-    bool paused;
-    bool onLevel;
     Canvas canvas;
 
 	// Use this for initialization
 	void Start ()
     {
-        paused = false;
-        onLevel = false;
         canvas = GetComponent<Canvas>();
-        canvas.enabled = false;
         LevelCanvas.enabled = false;
     }
 	
@@ -27,36 +22,13 @@ public class StartMenuScript : MonoBehaviour
     {
 	    if(Input.GetButtonDown("Cancel"))
         {
-            if (onLevel)
-            {
-                Back();
-            }
-            else
-            {
-                paused = !paused;
-                PauseMenu();
-            }
+            
         }
 	}
 
-    void PauseMenu()
+    public void StartGame()
     {
-        if(paused)
-        {
-            Time.timeScale = 0;
-            canvas.enabled = !canvas.enabled;
-        }
-        else
-        {
-            Time.timeScale = 1;
-            canvas.enabled = !canvas.enabled;
-        }
-    }
-
-    public void Resume()
-    {
-        paused = !paused;
-        PauseMenu();
+        SceneManager.LoadScene(1);
     }
 
     public void Quit()
@@ -70,7 +42,6 @@ public class StartMenuScript : MonoBehaviour
 
     public void Level()
     {
-        onLevel = true;
         canvas.enabled = false;
         LevelCanvas.enabled = true;
     }
@@ -107,7 +78,6 @@ public class StartMenuScript : MonoBehaviour
 
     public void Back()
     {
-        onLevel = false;
         canvas.enabled = true;
         LevelCanvas.enabled = false;
     }
