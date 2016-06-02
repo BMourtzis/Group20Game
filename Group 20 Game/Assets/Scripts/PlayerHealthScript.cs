a57d0472d230db1d4063ca3490c0bf3289b511b8
BMourtzis <vasilis.mourtzis@outlook.com>
2016-06-02 22:08:57 +1000

    Fixed Health and Ammo Text Issue

--- Group 20 Game/Assets/Scripts/PlayerHealthScript.cs
+++ Group 20 Game/Assets/Scripts/PlayerHealthScript.cs
@@ -6,56 +6,57 @@ public class PlayerHealthScript : MonoBehaviour
 
     [SerializeField]
     float HealthPoints;
+    [SerializeField]
+    HealthTextScript text;
 
     // Update is called once per frame
     void Update()
     {
-        if(HealthPoints > 101)
+        if (HealthPoints > 101)
         {
             HealthPoints -= Time.deltaTime;
         }
         else if (HealthPoints <= 0)
         {
-			HealthPoints = 0;
+            HealthPoints = 0;
             //End game;
         }
+
+        text.setHealth(HealthPoints);
     }
 
     //Decreases the Health of the Player
     public void TakeDamage(float damage, bool right)
     {
         HealthPoints -= damage;
+        text.takeDamage(HealthPoints);
         Vector2 force;
         if (right) { force = transform.right; }
-        else { force = transform.right*-1; }
+        else { force = transform.right * -1; }
         GetComponent<Rigidbody2D>().AddForce(force * 400);
     }
 
     public void TakeDamage(float damage)
     {
         HealthPoints -= damage;
+        text.takeDamage(HealthPoints);
     }
 
     // Used in Guan's level to get hurt by bomb.
-    void OnCollisionEnter2D (Collision2D bomb)
-	{
-		bool right;
-		if (Random.Range (0.0f, 1.0f) < 0.5f)
-			right = false;
-		else
-			right = true;
-		if (bomb.gameObject.tag == "Bomb")
-			TakeDamage (20, right);
-	}
+    void OnCollisionEnter2D(Collision2D bomb)
+    {
+        bool right;
+        if (Random.Range(0.0f, 1.0f) < 0.5f)
+            right = false;
+        else
+            right = true;
+        if (bomb.gameObject.tag == "Bomb")
+            TakeDamage(20, right);
+    }
 
     //Increases the Health of the Player
     public void RecoverHealth(float health)
     {
         HealthPoints += health;
-    }
-
-    public float getHealth()
-    {
-        return HealthPoints;
     }
 }
