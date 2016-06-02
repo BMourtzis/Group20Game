936d458ab09b1844dec397b2a57644ba578011ef
platinumgamer <platinumgamer@hotmail.com>
2016-06-02 21:44:13 +1000

    end text for loss

--- Group 20 Game/Assets/Scripts/ShipHealth.cs
+++ Group 20 Game/Assets/Scripts/ShipHealth.cs
@@ -6,6 +6,8 @@ public class ShipHealth : MonoBehaviour {
 
 	public int healthPoints;
 	public Text healthText;
+	public Text endText;
+	//public Spawner spawner
 
 	void OnCollisionEnter2D (Collision2D bomb) {
 		if (bomb.gameObject.tag == "Bomb") {
@@ -15,8 +17,10 @@ public class ShipHealth : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		if (healthPoints < 0)
+		if (healthPoints < 0) {
 			healthPoints = 0;
+			endText.text = "Your ship was destroyed!";
+		}
 		healthText.text = "Ship HP: " + healthPoints;
 	}
 
