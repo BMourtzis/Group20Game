fb855a63ec53d703911b1e203dcb58b95f014a5a
platinumgamer <platinumgamer@hotmail.com>
2016-06-02 20:54:59 +1000

    added hints to the beginning

--- Group 20 Game/Assets/Scripts/Spawner.cs
+++ Group 20 Game/Assets/Scripts/Spawner.cs
@@ -8,6 +8,7 @@ public class Spawner : MonoBehaviour {
 
 	public GameObject bomb;
 	public Text bombRemainder;
+	public Text guideText;
 	public float dragReduction;
 	public float dropDelayReduction;
 	public float minDrag;
@@ -29,6 +30,14 @@ public class Spawner : MonoBehaviour {
 	IEnumerator Spawn()
 	{
 		// Bombs will drop quicker and more often as time progresses.
+		guideText.text = "Shoot the bombs before it destroys your ship!";
+		yield return new WaitForSeconds (4);
+		guideText.text = "There is ammo around the turrets of the ship!";
+		yield return new WaitForSeconds (4);
+		guideText.text = "Good luck!";
+		yield return new WaitForSeconds (2);
+		guideText.text = "";
+
 		while (numOfBombs > 0) {
 			Vector3 spawnPosition = new Vector3 (Random.Range (-3, 94), transform.position.y, 0);
 			if (true) {
