801ef74516832f2d75d91c45dad28aea49f3ad3b
BMourtzis <vasilis.mourtzis@outlook.com>
2016-06-02 21:41:01 +1000

    Basic Start Menu

---
+++ Group 20 Game/Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuScript : MonoBehaviour
+{
+    bool paused;
+    Canvas canvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paused = false;
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetButtonDown("Cancel"))
+        {
+            Debug.Log(paused);
+            paused = !paused;
+            StartMenu();
+        }
+	}
+
+    void StartMenu()
+    {
+        if(paused)
+        {
+            Time.timeScale = 0;
+            canvas.enabled = !canvas.enabled;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            canvas.enabled = !canvas.enabled;
+        }
+    }
+}
