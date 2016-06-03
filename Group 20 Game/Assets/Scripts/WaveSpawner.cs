b8c0d7390a0e57e274b1b52f28b40d344788372c
platinumgamer <platinumgamer@hotmail.com>
2016-06-03 22:07:37 +1000

    remainder text improved

--- Group 20 Game/Assets/Scripts/WaveSpawner.cs
+++ Group 20 Game/Assets/Scripts/WaveSpawner.cs
@@ -9,11 +9,18 @@ public class WaveSpawner : MonoBehaviour
 	public Text guideText;
 
 	int enemies;
+	int alive;
 
 	void Start()
 	{
 		enemies = 40;
 		StartCoroutine(Spawn());
+		alive = enemies;
+	}
+
+	void Update() {
+		if (alive < 0)
+			guideText.text = "You killed them all";
 	}
 
 	IEnumerator Spawn()
@@ -28,20 +35,26 @@ public class WaveSpawner : MonoBehaviour
 
 		while (enemies > 0)
 		{
-			Vector3 spawnPosition = new Vector3(-40, transform.position.y, 0);
+			Vector3 spawnPositionLeft = new Vector3(Random.Range(-40, -30), transform.position.y, 0);
+			Vector3 spawnPositionRight = new Vector3(Random.Range(30, 40), transform.position.y, 0);
 			if (true)
 			{
-				Instantiate(enemy, spawnPosition, transform.rotation);
+				Instantiate(enemy, spawnPositionLeft, transform.rotation);
+				Instantiate(enemy, spawnPositionRight, transform.rotation);
 			}
 			enemies -= 1;
-			enemyRemainder.text = "Enemies: " + enemies;
+			enemyRemainder.text = "Enemies: " + alive;
 
-			yield return new WaitForSeconds(1);
+			yield return new WaitForSeconds(Random.Range(0.5f, 2.0f));
 		}
 	}
 
 	public int getNumOfEnemies()
 	{
 		return enemies;
+	}
+
+	public void setAlive(int killed) {
+		alive -= killed;
 	}
 }
---
+++ Group 20 Game/Assets/isDead.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isDead : MonoBehaviour {
+
+	private GameObject controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GameObject.FindGameObjectWithTag ("GameController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.GetComponent<EnemyHealth> ().getHealth () < 0)
+			controller.GetComponent<WaveSpawner>().setAlive(1);
+	}
+}
